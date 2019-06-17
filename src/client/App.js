ad536bb0b5a06efeeb555979eaf52e6391175ea4
Mia Baz <themiabaz@gmail.com>
2019-06-17 16:52:49 -0400

    split NavButtons component from Header, add react-router-dom imports

--- src/client/App.js
+++ src/client/App.js
@@ -1,15 +1,30 @@
 import "../bootstrap/css/bootstrap.css";
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Header from "./Components/Header";
 import About from "./Components/About";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <About />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <About />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+{
+  /* <li><Link to={'/create'}>Create</Link></li>
+<li><Link to={'/index'}>List</Link></li>
+</ul>
+<hr />
+<Switch>
+  <Route exact path='/create' component={ Create } />
+  <Route path='/index' component={ Index } />
+</Switch> */
+}
--- src/client/Components/Header.js
+++ src/client/Components/Header.js
@@ -1,7 +1,5 @@
 import React from "react";
 import HeadLogo from "./HeadLogo";
-import { Button } from "react-bootstrap";
-import resume from "assets/Mia.Baz.pdf";
 import "bootstrap/css/bootstrap.css";
 import "bootstrap/css/header.css";
 
@@ -12,40 +10,6 @@ function Header() {
         <HeadLogo />
       </div>
       <div className="navbar-center">welcome to bazb0t.dev!</div>
-      <div className="navbar-right">
-        <Button
-          variant="primary"
-          size="lg"
-          href="https://github.com/bazb0t"
-          target="_blank"
-          rel="noopener noreferrer"
-          value="github"
-        >
-          github
-        </Button>
-
-        <Button
-          variant="primary"
-          size="lg"
-          href="https://linkedin.com/in/miabaz"
-          target="_blank"
-          rel="noopener noreferrer"
-          value="linkedIn"
-        >
-          linkedIn
-        </Button>
-
-        <Button
-          variant="primary"
-          size="lg"
-          href={resume}
-          target="_blank"
-          rel="noopener noreferrer"
-          value="resume"
-        >
-          resume
-        </Button>
-      </div>
     </div>
   );
 }
